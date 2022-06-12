d8dc5dac531e5afc416868a1f4f0045a4eb200f4
andrijastt <89318511+andrijastt@users.noreply.github.com>
2022-06-12 13:07:41 +0200

    Server povezan sa bazom

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose, { mongo } from 'mongoose';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/projekatDB'); 
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('db connected')
+})
+
+const router = express.Router();
+
+app.use('/', router)
 app.listen(4000, () => console.log(`Express server running on port 4000`));
